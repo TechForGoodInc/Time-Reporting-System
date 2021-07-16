efecf8b3a8e92c162d281dcd2894080df85f3b5c
Harmmike <harmmike46@gmail.com>
2021-07-16 18:49:08 +0800

    Added logic in logoutBtn.jsx to prevent auto-refresh after logout.

--- src/components/logoutBtn.jsx
+++ src/components/logoutBtn.jsx
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 
 class LogoutBtn extends Component {
 
+    //Get button and add listener to prevent page refresh on logout
+    componentDidMount() {
+        document.getElementById('logoutBtn').addEventListener('click', btn => {
+            btn.preventDefault();
+        });
+    }
+
+    //Remove previously added listener - just in case button is disabled in future
+    componentWillUnmount() {
+        document.getElementById('logoutBtn').removeEventListener('click', btn => {
+            btn.preventDefault();
+        });
+    }
+
     render() {
         return (
             <React.Fragment>
-                <button onClick={this.props.handleLogout} style={{ float: 'right', color: 'white', background: 'red' }}>Log out</button>
+                <button id={'logoutBtn'} onClick={this.props.handleLogout} style={{ float: 'right', color: 'white', background: 'red' }}>Log out</button>
             </React.Fragment>
         );
     }
