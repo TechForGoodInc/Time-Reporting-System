5730433ddc48121a08f1d973706a38085bff6047
ntaylor562 <nathan.taylor562@gmail.com>
2021-07-17 00:01:38 -0700

    Removed unnecessary functions from logoutBtn

--- src/components/logoutBtn.jsx
+++ src/components/logoutBtn.jsx
@@ -2,20 +2,6 @@ import React, { Component } from 'react';
 
 class LogoutBtn extends Component {
 
-    //Get button and add listener to prevent page refresh on logout
-    componentDidMount() {
-        document.getElementById('logoutBtn').addEventListener('click', btn => {
-            btn.preventDefault();
-        });
-    }
-
-    //Remove previously added listener - just in case button is disabled in future
-    componentWillUnmount() {
-        document.getElementById('logoutBtn').removeEventListener('click', btn => {
-            btn.preventDefault();
-        });
-    }
-
     render() {
         return (
             <React.Fragment>
