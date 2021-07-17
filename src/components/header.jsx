d7bb5c057fddd9f24c1f11251bbbb7cfcefd4c1e
ntaylor562 <nathan.taylor562@gmail.com>
2021-07-17 00:45:46 -0700

    Disabled logout button
    Testing logout is inconsistent which led to a false fix. A future update will address the entire authentication process enabling the logout button again.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ class Header extends Component {
     render() {
         return (
             <div>
-                <LogoutBtn handleLogout={(e) => { this.props.handleLogout(e) }}/>
+                {/*<LogoutBtn handleLogout={(e) => { this.props.handleLogout(e) }}/> Disabled until fixed */}
                 <h3 id="title">TFG Time Reporting System - {this.props.email}</h3>
             </div>
         );
