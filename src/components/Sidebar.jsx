33ead43edd1a350731bbe30d95fd9f57b3e3785f
Natalia Kusmirek <67024662+nataliakusmirek@users.noreply.github.com>
2021-08-10 12:47:17 -0400

    Sidebar Styling
    Headers are now green to match the theme, center-aligned, clean background with the TFG logo on top. TFG logo is clickable, which forwards the user to the TFG website as well.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,20 +4,30 @@ import { slide as Menu } from "react-burger-menu";
 export default props => {
   return (
     <Menu {...props}>
+      <a className="menu-item" href="https://techforgoodinc.org/">
+        <center>
+          <img src='https://techforgoodinc.org/images/TechForGood_Logo.png' width='100' height='50'></img>
+        </center>
+      </a>
+      
+      <a className="menu-item">
+        <p style={{ color: '#373a47' }}>----</p>
+      </a>
+
       <a className="menu-item" href="/">
-        Home
+        <p style={{ color: '#A8F0B7', textAlign:'center', backgroundColor:'#595969' }}><b>Home</b></p>
       </a>
 
       <a className="menu-item" href="/...">
-        Activity
+        <p style={{ color: '#77CB88', textAlign: 'center', backgroundColor:'#595969' }}>Activity</p>
       </a>
 
       <a className="menu-item" href="/...">
-        Calendar
+        <p style={{ color: '#77CB88', textAlign: 'center', backgroundColor:'#595969' }}>Calendar</p>
       </a>
 
       <a className="menu-item" href="/...">
-        Logout
+        <p style= {{ color: '#77CB88', textAlign: 'center', backgroundColor:'#595969' }}><b>Logout</b></p>
       </a>
 
     </Menu>
