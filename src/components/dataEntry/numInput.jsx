9c169634b0a9185da2ffeba39b5b43120d206b4c
Jack Yang <yyang17@wpi.edu>
2021-08-10 14:15:07 -0400

    Fix min and max limit for edit feature The limitation is now 0-12 hours

--- src/components/dataEntry/numInput.jsx
+++ src/components/dataEntry/numInput.jsx
@@ -5,7 +5,7 @@ class NumInput extends Component {
     render() {
         return (
             <React.Fragment>
-                <input type="number" id="hours" name="hours" style={{ verticalAlign: 'middle', width: '60px' }} placeholder="8" defaultValue={this.props.defaultValue} min={this.props.minimum} max={this.props.maximum} required onChange={this.props.changeHandler} step="0.01" />
+                <input type="number" id="hours" name="hours" style={{ verticalAlign: 'middle', width: '60px' }} placeholder="8" defaultValue={this.props.defaultValue} min={this.props.minimum ? this.props.minimum : 0} max={this.props.maximum ? this.props.maximum : 12} required onChange={this.props.changeHandler} step="0.01" />
             </React.Fragment>
         );
     }
