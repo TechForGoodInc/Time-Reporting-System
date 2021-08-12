ac3a2f28ec8778a5fbae80b002d4172842541078
ntaylor562 <nathan.taylor562@gmail.com>
2021-08-12 14:08:52 -0700

    Merged main with release-candidate to bring release-candidate up to date
    Usually release-candidate is updated first but the last update was pushed to main instead of release-candidate.

--- src/components/history.jsx
+++ src/components/history.jsx
@@ -11,30 +11,16 @@ class History extends Component {
 
     loadMore() {
         let newBlocks = [...this.state.histBlocks];
-        if (newBlocks.length === 0) { //History not shown
-            let d1 = new Date();
-            d1.setHours(12, 0 - d1.getTimezoneOffset(), 0, 0);
-            newBlocks.push({
-                startDate: d1,
-                endDate: d1
-            })
-        }
-        else if (newBlocks.length < 7) { //Only showing the past week (or less)
-            let d1 = new Date(new Date().setDate(new Date().getDate() - newBlocks.length));
-            newBlocks.push({
-                startDate: d1,
-                endDate: d1
-            })
-        }
-        else { //Clicking show more history will begin to show entries by week
-            let d1 = new Date(new Date().setDate((new Date().getDate() - (newBlocks.length - 7 + 2) * 7)+1));
-            let d2 = new Date(new Date(d1).setDate(d1.getDate() + 6));
+        let now = new Date();
+        
+        //Getting start and end of the week we're loading
+        let d1 = new Date(new Date().setDate(now.getDate() - now.getDay() - newBlocks.length * 7) + 1);
+        let d2 = new Date(new Date(d1).setDate(d1.getDate() + 6));
 
-            newBlocks.push({
-                startDate: d1,
-                endDate: d2
-            })
-        }
+        newBlocks.push({
+            startDate: d1,
+            endDate: d2
+        })
 
         this.setState({
             histBlocks: newBlocks
@@ -50,7 +36,7 @@ class History extends Component {
                     <ul style={{ listStyleType: 'none', paddingLeft: '0px' }}>
                         {this.state.histBlocks.map((data, index) => {
                             return <li key={index} style={{ marginBottom: '20px' }} >
-                                <HistoryBlock startDate={data.startDate} endDate={data.endDate} getEntries={this.props.getEntries} postData={this.props.postData} delete_data={this.props.delete_data} />
+                                <HistoryBlock startDate={data.startDate} endDate={data.endDate} getEntries={this.props.getEntries} postData={this.props.postData} delete_data={this.props.delete_data} nBlock={index} />
                             </li>
                         })}
                     </ul>
